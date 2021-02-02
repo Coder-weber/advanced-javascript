e2e0f20ab6c924cf8870cc167b0f319b8e9537a2
Coder-weber <juwel14310007@gmail.com>
2021-02-02 15:04:13 +0600

    == vs ===

--- doubleVthriple.js
+++ doubleVthriple.js
@@ -1,3 +1,13 @@
+
+// // 0 == false   // true
+// 0 === false  // false, because they are of a different type
+// 1 == "1"     // true, automatic type conversion for value only
+// 1 === "1"    // false, because they are of a different type
+// null == undefined // true
+// null === undefined // false
+// '0' == false // true
+// '0' === false // false
+
 let  num1=false;
 let num2='0';
 if(num1==num2){
